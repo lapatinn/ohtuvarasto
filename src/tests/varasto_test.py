8c89c3eeded96090b77750185ee6dfd48a8f79d8
Lauri <tikkalauri1@gmail.com>
2025-10-30 17:14:50 +0200

    Add coverage tool. Add additional tests to varasto_test.py for 100% branch coverage

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,38 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_negatiivinen_tilavuus_korjataan(self):
+        self.varasto_negatiivnen_tilavuus = Varasto(-10)
+
+        self.assertAlmostEqual(self.varasto_negatiivnen_tilavuus.tilavuus, 0)
+
+    def test_negatiivinen_alkusaldo_korjataan(self):
+        self.varasto_neg_alkusaldo = Varasto(10, -10)
+
+        self.assertAlmostEqual(self.varasto_neg_alkusaldo.saldo, 0)
+
+    def test_tyhja_lisays_ei_palauta(self):
+        # tyhjä TAI negatiivinen lisäys
+        self.assertAlmostEqual(self.varasto.lisaa_varastoon(-10), None)
+
+    def test_lisays_suurempi_kuin_mahtuu(self):
+        # jos lisäys > tilavuus-saldo -> saldo = tilavuus
+
+        # Lisätään enemmän kuin mahtuu:
+        self.varasto.lisaa_varastoon(self.varasto.paljonko_mahtuu() + 1)
+
+        # Tarkistetaan onko täynnä:
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_otetaan_negatiivinen_maara_palauttaa_0(self):
+        self.assertAlmostEqual(self.varasto.ota_varastosta(-10), 0)
+
+    def test_otetaan_enemman_kuin_saldo_palautta_koko_saldon(self):
+        self.varasto.ota_varastosta(self.varasto.saldo + 1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_merkkijonoesitys(self):
+        # emt pitäiskö tän tarkistaakki jotain mutta tästä selviää
+        # ainakin että str metodi ei kaada koko ohjelmaa :D
+        str(self.varasto)
